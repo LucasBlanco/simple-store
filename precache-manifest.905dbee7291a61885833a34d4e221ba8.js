7779bc82ab1b3debe4c5b3420f11a0189da1d3aa
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2020-12-31 09:50:50 -0300

    Updates

---
+++ precache-manifest.905dbee7291a61885833a34d4e221ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a221114154095e4b5021f6b11566f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2728c25d8fbff338df91",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "ae4364fd46adbaa14246",
+    "url": "./static/js/2.737fc308.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.737fc308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2728c25d8fbff338df91",
+    "url": "./static/js/main.abace9f4.chunk.js"
+  },
+  {
+    "revision": "d864ff475359a6a1c20d",
+    "url": "./static/js/runtime-main.333b18a6.js"
+  }
+]);
